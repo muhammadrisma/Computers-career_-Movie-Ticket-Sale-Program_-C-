b24b25161a1932428c5b1217001df3449e77954a
muhammadrisma <86287735+muhammadrisma@users.noreply.github.com>
2021-11-06 19:46:00 +0700

    Add files via upload

---
+++ Assignment8_Muhammad Risma_001202100048.cpp
@@ -0,0 +1,83 @@
+#include <iostream> 
+#include <iomanip>
+#include <string>
+ 
+using namespace std;
+  
+int main()
+{
+       
+    string movieName;
+    double adultTicketPrice;
+    double childTicketPrice;
+    int noOfAdultTicketsSold;
+    int noOfChildTicketsSold;
+    double percentDonation;
+    double grossAmount;
+    double amountDonated;
+    double netSaleAmount;
+
+    cout << fixed << showpoint << setprecision(2);  
+
+    cout << "Enter the movie name: ";               
+    getline(cin, movieName);                        
+    cout << endl;
+
+    cout << "Enter the price of an adult ticket: "; 
+    cin >> adultTicketPrice;                        
+    cout << endl;
+
+    cout << "Enter the price of a child ticket: ";  
+    cin >> childTicketPrice;                        
+    cout << endl;
+
+    cout << "Enter the number of adult tickets "
+         << "sold: ";                               
+    cin >> noOfAdultTicketsSold;                    
+    cout << endl;
+
+    cout << "Enter the number of child tickets " 
+         << "sold: ";                               
+    cin >> noOfChildTicketsSold;                    
+    cout << endl;
+ 
+    cout << "Enter the percentage of donation: ";   
+    cin >> percentDonation;                         
+    cout << endl << endl;
+ 
+                   
+    grossAmount = adultTicketPrice * noOfAdultTicketsSold +
+                  childTicketPrice * noOfChildTicketsSold; 
+
+                   
+    amountDonated = grossAmount * percentDonation / 100; 
+
+    netSaleAmount = grossAmount - amountDonated;    
+
+           
+    cout << "-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*"
+         << "-*-*-*-*-*-*-*-*-*-*-*-*" << endl;
+    cout << setfill('.') << left << setw(35) << "Movie Name: "
+         << right << " " << movieName << endl;
+    cout << left << setw(35) << "Number of Tickets Sold: "
+         << setfill(' ') << right << setw(10)
+         << noOfAdultTicketsSold + noOfChildTicketsSold
+         << endl;
+    cout << setfill('.') << left << setw(35)
+         << "Gross Amount: "
+         << setfill(' ') << right << " $"
+         << setw(8) << grossAmount << endl;
+    cout << setfill('.') << left << setw(35)
+         << "Percentage of Gross Amount Donated: "
+         << setfill(' ') << right
+         << setw(9) << percentDonation << '%' << endl;
+    cout << setfill('.') << left << setw(35) 
+         << "Amount Donated: "
+         << setfill(' ') << right << " $"
+         << setw(8) << amountDonated << endl;
+    cout << setfill('.') << left << setw(35) << "Net Sale: "
+         << setfill(' ') << right << " $"
+         << setw(8) << netSaleAmount << endl;
+
+    return 0;
+}
